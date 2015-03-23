452ef79f182d11c5264cf4770e1755ced17a6531
YeThuSoe <mr.yethusoe@gmail.com>
2015-03-23 11:09:06 +0630

    update

--- qa-theme.php
+++ qa-theme.php
@@ -8,5 +8,6 @@ class qa_html_theme extends qa_html_theme_base
 		qa_html_theme_base::head_css();
 		$this->output('<link rel="shortcut icon" href="'.$this->rooturl.'logo.jpg" />');
 		$this->output('<link href="http://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet" type="text/css">');
+		$this->output('<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">');
 	}
 }
